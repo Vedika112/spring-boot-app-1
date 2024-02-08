b75f9706b9c7f6dde1d126238c57f05e6d04ee38
Vedika112 <88924992+Vedika112@users.noreply.github.com>
2024-02-08 23:35:13 +0530

    Update SpringBootAppApplication.java

--- demo/src/main/java/com/app/demo/SpringBootAppApplication.java
+++ demo/src/main/java/com/app/demo/SpringBootAppApplication.java
@@ -3,7 +3,7 @@ package com.app.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
