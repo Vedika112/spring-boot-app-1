9287d9bab904571877026c226af6ff5179ca9bf3
Vedika112 <88924992+Vedika112@users.noreply.github.com>
2024-02-08 23:20:16 +0530

    Add files via upload

---
+++ src/main/java/com/app/demo/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.app.demo.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String s) {
+        super(s);
+    }
+
+    public ResourceNotFoundException(){
+        super("Resource Not found !!");
+    }
+}
