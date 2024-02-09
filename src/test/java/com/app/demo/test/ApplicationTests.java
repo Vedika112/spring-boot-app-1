48dc67e443e2188a01a6a494f952f6281c0c3f33
Vedika112 <vedikachandaliya@gmail.com>
2024-02-09 13:55:13 +0530

    test file commit

--- src/test/java/com/app/demo/test/ApplicationTests.java
+++ src/test/java/com/app/demo/test/ApplicationTests.java
@@ -1,2 +1,28 @@
-package com.app.demo.test;public class ApplicationTests {
+package com.app.demo.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class ApplicationTests {
+
+    private String hello;
+
+    @BeforeEach
+    void setUp() {
+        hello = "Hello";
+    }
+
+    @Test
+    void testPrintHello() {
+        System.out.println(hello);
+        assertThat(hello).isNotNull();
+    }
 }
+
